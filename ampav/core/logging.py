64e280d429907a9dc13f77e85f6660b04711ef43
Brian Wheeler <bdwheele@iu.edu>
2026-04-02 11:18:06 -0400

    initial code to test with

---
+++ ampav/core/logging.py
@@ -0,0 +1,22 @@
+import logging
+import logging.handlers
+
+
+LOG_FORMAT = "%(asctime)s [%(levelname)-8s] (%(filename)s:%(lineno)d:%(process)d) %(message)s"
+
+class ListLoggingHandler(logging.Handler):
+    """
+    Docstring for ListLoggingHandler
+    """
+    def __init__(self, log: list):
+        """
+        Initialize the List Logging Handler
+
+        :param log: List to append new logging messages to
+        :type log: list
+        """
+        self.log = log
+
+    def emit(self, record):
+        self.log.append(self.format(record))
+
---
+++ ampav/core/utils.py
@@ -0,0 +1,39 @@
+"""
+General purpos utilities
+"""
+
+def duration2hhmmss(duration: float) -> str:
+    """
+    Take a duration in seconds and convert it to hh:mm:ss.sss
+    
+    :param timestamp: Number of seconds to convert
+    :return: Human-readable duration string
+    :rtype: str
+    """
+    hours = int(duration / 3600)
+    duration -= hours * 3600
+    minutes = int(duration / 60)
+    seconds = duration - minutes * 60
+    return f"{hours:02d}:{minutes:02d}:{seconds:06.3f}"
+
+
+def hhmmss2seconds(hhmmss: str) -> float:
+    """
+    Convert a duration string in the format of hh:mm:ss.sss to seconds
+    
+    :param hhmmss: Human readable duration string
+    :return: Number of seconds represented by the string
+    :rtype: float
+    """
+    parts = hhmmss.split(":")
+    if len(parts) == 1:
+        # looks like it was just seconds
+        return float(hhmmss)
+    elif len(parts) == 2:
+        # mm:ss
+        return int(parts[0]) * 60 + float(parts[1])
+    elif len(parts) == 3:
+        # hh:mm:ss
+        return int(parts[0]) * 3600 + int(parts[1]) * 60 + float(parts[2])
+    else:
+        raise ValueError(f"Can't recognize format of {hhmmss}")
