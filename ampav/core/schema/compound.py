8bd71d467aa2150e11c74b0e4731883b45928e9d
Brian Wheeler <bdwheele@iu.edu>
2026-05-13 12:55:14 -0400

    formatting

--- ampav/core/schema/compound.py
+++ ampav/core/schema/compound.py
@@ -1,7 +1,6 @@
 from pydantic import Field
 from typing import Callable, Literal, Annotated, Union, Any
 from .basemodel import AmpAVBaseModel
-
 from .av_metadata import AVMetadata
 from .transcript import Transcript
 from .raw import RawData, RawBinary
@@ -14,8 +13,7 @@ class CompoundOutput(AmpAVBaseModel):
     ampav_format: Literal['compound/1'] = 'compound/1'
     outputs: dict[str, OutputTypes] = Field(default_factory=dict, description="Storage of multiple outputs")
 
-
-    def find_by_type(self, output_type: object) -> list[str]:
+    def find_by_type(self, output_type: object) -> list[str]:    
         """Get the output keys for any object that is the same as
            the output type"""
         return [k for k, v in self.outputs.items() if type(output_type) == type(v)]
