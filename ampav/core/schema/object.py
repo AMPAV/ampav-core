a766ab745bda1503701397a4dffbcd1427b1c131
Brian Wheeler <bdwheele@iu.edu>
2026-07-29 13:12:14 -0400

    update detected object to handle faces, move render into core so it's available for everyone.

--- ampav/core/schema/object.py
+++ ampav/core/schema/object.py
@@ -1,18 +1,38 @@
+from enum import StrEnum, auto
+
 from pydantic import Field
 from typing import Any, Literal
 from .basemodel import AmpAVBaseModel
 from .segments import ConfidenceSegment, Segment
 from .image import Image, BoundingBox
 
+class DetectedObjectType(StrEnum):
+    OBJECT=auto()
+    "An object"
+    FACE=auto()
+    "A Face"
+    OTHER=auto()
+    "Something that doesn't fit the above categories"
+    UNKNOWN=auto()
+    "Unknown or unset"
+
+
+
+class DetectedObjectInstance(ConfidenceSegment):
+    images: list[Image] = Field(default_factory=list)
+    "Images found in the segment"
+
 
 class DetectedObject(AmpAVBaseModel):
+    type: DetectedObjectType = DetectedObjectType.UNKNOWN
+    "Type of detected object"
     image: Image | None =None
-    "Image of the object"
+    "Typical Image of the object"
     text: str | None = None
     "text name of the object"
     label: str | None = None
     "Tool-specific label name"
-    instances: list[ConfidenceSegment] = Field(default_factory=list)
+    instances: list[DetectedObjectInstance] = Field(default_factory=list)
     "Instances where the object appears"
     tool_private: dict[str, Any] | None = None
     "Additional tool-specific fields"
