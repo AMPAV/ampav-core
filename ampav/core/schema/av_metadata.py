a5c8c1380aedb4a80e31803f0accea0507b1445a
Brian Wheeler <bdwheele@iu.edu>
2026-08-19 10:07:11 -0400

    work around circular dependencies

--- ampav/core/schema/av_metadata.py
+++ ampav/core/schema/av_metadata.py
@@ -5,7 +5,7 @@ from typing import Annotated, Any
 from annotated_types import Len, Literal
 from pathlib import Path
 
-from ampav.core.utils import dump_data
+
 from .basemodel import AmpAVBaseModel
 import av
 import av.container
@@ -173,6 +173,7 @@ def cli_probe_media():
     # putting the import here fixes a circular dependency when the module is 
     # used generally.
     from ampav.core.schema.tool import ToolOutput
+    from ampav.core.utils import dump_data
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', help="Filename to probe")
     parser.add_argument('output', type=Path, help="Output file")
